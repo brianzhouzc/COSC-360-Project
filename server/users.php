dda9a351cc671ade0528a0ba50298c28e7f2374b
Brian Zhou <brianzhouzc@gmail.com>
2021-03-30 21:06:51 -0700

    Initial commit

---
+++ server/users.php
@@ -0,0 +1,16 @@
+<?php
+include 'database.php';
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+
+    switch ($action) {
+        case "login":
+            break;
+        case "logout":
+            break;
+        case "register":
+            break;
+        case "forgot":
+            break;
+    }
+}
