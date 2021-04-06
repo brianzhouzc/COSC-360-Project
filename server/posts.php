24a4bbc643d71a8e05aae62e8c9b47ad48026a86
Brian Z <brianzhouzc@gmail.com>
2021-04-06 14:26:56 -0700

    Update posts.php

--- server/posts.php
+++ server/posts.php
@@ -19,12 +19,36 @@ function createPost($connection, $username, $session, $content)
 function updatePost($connection, $post_id, $username, $session, $content)
 {
     if (authenticateUser($connection, $username, $session)) {
+        $post = getPostById($connection, $post_id);
+        if (strcmp($post['username'], $username) == 0) {
+            //$user = getUserByName($connection, $username);
+            $sql = "UPDATE posts SET content = ? WHERE post";
+            $stmt = $connection->prepare($sql);
+            $stmt->bind_param("ss", $username, $content);
+            $stmt->execute();
+            // updated
+        } else {
+            // user does not own post
+        }
+    } else {
+        // smth else
+    }
+}
 
-        //$user = getUserByName($connection, $username);
-        $sql = "UPDATE posts SET content = ? WHERE post";
-        $stmt = $connection->prepare($sql);
-        $stmt->bind_param("ss", $username, $content);
-        $stmt->execute();
+function removePost($connection, $post_id, $username, $session)
+{
+    if (authenticateUser($connection, $username, $session)) {
+        $post = getPostById($connection, $post_id);
+        if (strcmp($post['username'], $username) == 0) {
+            //$user = getUserByName($connection, $username);
+            $sql = "DELETE FROM posts WHERE id = ?;";
+            $stmt = $connection->prepare($sql);
+            $stmt->bind_param("i", $post_id);
+            $stmt->execute();
+            // deleted
+        } else {
+            // user does not own post
+        }
     } else {
         // smth else
     }
@@ -42,6 +66,13 @@ function getPostById($connection, $post_id)
     return $post;
 }
 
-function getPostsByUsername()
+function getPostsByUsername($connection, $username)
 {
+    $sql = "SELECT * FROM posts WHERE username = ?;";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("i", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    return $result;
 }
