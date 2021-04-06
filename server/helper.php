f5b5d0ec4e8bc757d45aa55eb77093861f83aa76
Brian Z <brianzhouzc@gmail.com>
2021-04-05 23:08:27 -0700

    password reset logic

--- server/helper.php
+++ server/helper.php
@@ -5,3 +5,13 @@ function errorMsg($code, $status, $message)
     $response = array("code"=>$code, "status"=>$status, "message"=>$message);
     return (json_encode($response));
 }
+
+function generateRandomString($length = 6) {
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
--- server/users.php
+++ server/users.php
@@ -35,18 +35,68 @@ if (isset($_POST['action'])) {
         case "forgot":
             if (isset($_POST['email'])) {
                 $email = $_POST['email'];
+
                 if (exsist($connection, null, $email)) {
-                    // SEND RESET EMAIL SOMEHOW
+                    if (isset($_POST['token'])) {
+                        $token = $_POST['token'];
+
+                        if (isset($_POST['password'])) {
+                            $password = md5($_POST['password']);
+                            $sql = "SELECT token, token_timestamp FROM users WHERE $email = ?;";
+                            $stmt = $connection->prepare($sql);
+                            $stmt->bind_param("ss", $email, $token);
+                            $stmt->execute();
+                            $result = $stmt->get_result();
+                            $user = $result->fetch_assoc();
+
+                            if (isset($user['token']) && isset($user['token_timestamp'])) {
+                                if (strcmp($token, $user['token']) == 0) {
+                                    //code sent less than an hour;
+                                    if (time() - $user['token_timestamp'] < 3600) {
+                                        // Update password
+                                        $sql2 = "UPDATE users SET password = ? WHERE email = ?";
+                                        $stmt2 = $connection->prepare($sql2);
+                                        $stmt2->bind_param("ss", $password, $email);
+                                        $stmt2->execute();
+                                        /**** SEND CONFIRM RESPONSE ****/
+                                    } else {
+                                        // token expired. reset token and token_timestamp to null
+                                        $sql2 = "UPDATE users SET token = NULL, token_timestamp = NULL WHERE email = ?;";
+                                        $stmt2 = $connection->prepare($sql2);
+                                        $stmt2->bind_param("s", $email);
+                                        $stmt2->execute();
+                                        exit(errorMsg(400, "fail", "Password reset token expired"));
+                                    }
+                                } else {
+                                    exit(errorMsg(400, "fail", "Invalid reset token"));
+                                }
+                            } else {
+                                exit(errorMsg(400, "fail", "No password reset request initiated"));
+                            }
+                        } else {
+                            exit(errorMsg(400, "fail", "Missing password"));
+                        }
+                    } else {
+                        $token = generateRandomString();
+                        $timestamp = time();
+                        $sql = "UPDATE users SET token = ?, token_timestamp = ? WHERE email = ?;";
+                        $stmt = $connection->prepare($sql);
+                        $stmt->bind_param("sis", $token, $timestamp, $email);
+                        $stmt->execute();
+                        // SEND RESET EMAIL SOMEHOW $token
+                        // SEND confirm message
+                    }
                 } else {
                     exit(errorMsg(400, "fail", "Email does not exsist"));
                 }
             }
-
             break;
+        default:
+            exit(errorMsg(400, "fail", "Invalid action"));
     }
 } else {
     $connection->close();
-    exit(errorMsg(400, "fail", "Missing action."));
+    exit(errorMsg(400, "fail", "Missing action"));
 }
 
 function exsist($connection, $username, $email)
