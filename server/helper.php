0da62358f2ea7adea7031287a2482183157f6332
Brian Z <brianzhouzc@gmail.com>
2021-04-06 21:08:19 -0700

    Make functions return response instead of exiting

--- server/helper.php
+++ server/helper.php
@@ -1,10 +1,22 @@
 <?php
 
-function errorMsg($status, $detail)
+function errorResponse($status, $detail)
 {
     $error = array("status" => $status, "detail" => $detail);
     $response = array("errors" => $error);
     return (json_encode($response));
+}
+
+function dataResponse($status, $detail, $data = null)
+{
+    if (isset($data)) {
+        if (!is_array($data))
+            $data = array($data);
+        $data = array("data" => (array("status" => $status, "detail" => $detail) + $data));
+    } else {
+        $data = array("data" => (array("status" => $status, "detail" => $detail)));
+    }
+    return (json_encode($data));
 }
 
 function generateRandomString($length = 6)
@@ -16,4 +28,15 @@ function generateRandomString($length = 6)
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
+}
+
+function isset_notempty($var)
+{
+    return (isset($var) && !empty($var));
+}
+
+function exitandclose($msg, $connection)
+{
+    $connection->close();
+    exit($msg);
 }
--- server/users.php
+++ server/users.php
@@ -6,56 +6,58 @@ if (isset($_POST['action'])) {
     $action = $_POST['action'];
     switch ($action) {
         case "login":
-            if (isset($_POST['username']) && isset($_POST['password'])) {
+            if (isset_notempty($_POST['username']) && isset_notempty($_POST['password'])) {
                 $username = $_POST['username'];
                 $password = $_POST['password'];
-                login($connection, $username, $password);
+                $response = login($connection, $username, $password);
+                exitandclose($response, $connection);
             } else {
-                exit(errorMsg(400, "Missing username/password"));
+                exit(errorResponse(400, "Missing username/password"));
             }
             break;
 
         case "logout":
-            if (isset($_POST['username']) && isset($_POST['session'])) {
+            if (isset_notempty($_POST['username']) && isset_notempty($_POST['session'])) {
                 $username = $_POST['username'];
                 $session = $_POST['session'];
-                logout($connection, $username, $session);
+                $response = logout($connection, $username, $session);
+                exitandclose($response, $connection);
             } else {
-                exit(errorMsg(400, "Missing username/session"));
+                exit(errorResponse(400, "Missing username/session"));
             }
             break;
 
         case "register":
-            if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
+            if (isset_notempty($_POST['username']) && isset_notempty($_POST['email']) && isset_notempty($_POST['password'])) {
                 $username = $_POST['username'];
                 $email = $_POST['email'];
                 $password = md5($_POST['password']);
                 $avatar = isset($_FILES['avatar']) ? $_FILES['avatar'] : null;
-
-                register($connection, $username, $email, $password, $avatar);
+                $response = register($connection, $username, $email, $password, $avatar);
+                exitandclose($response, $connection);
             } else {
-                exit(errorMsg(400, "Missing username/email/password"));
+                exit(errorResponse(400, "Missing username/email/password"));
             }
             break;
 
         case "forgot":
-            if (isset($_POST['email'])) {
+            if (isset_notempty($_POST['email'])) {
                 $email = $_POST['email'];
                 $token = $_POST['token'];
                 $password = $_POST['password'];
-
-                forgot($connection, $email, $token, $password);
+                $response = forgot($connection, $email, $token, $password);
+                exitandclose($response, $connection);
             } else {
-                exit(errorMsg(400, "Missing email"));
+                exit(errorResponse(400, "Missing email"));
             }
             break;
 
         default:
-            exit(errorMsg(400, "Invalid action"));
+            exit(errorResponse(400, "Invalid action"));
     }
 } else {
     $connection->close();
-    exit(errorMsg(400, "Missing action"));
+    exit(errorResponse(400, "Missing action"));
 }
 
 function login($connection, $username, $password)
@@ -68,12 +70,12 @@ function login($connection, $username, $password)
             $session = generateRandomString(255);
             updateSession($connection, $username, $session);
             //LOGGED IN, PASS $session to front end, store $session in sessionStorage;
-            echo (json_encode(array("session"=>$session)));
+            return dataResponse(200, "Successfully logged in", array("username" => $username, "session" => $session));
         } else {
-            exit(errorMsg(400, "Invalid username/password"));
+            return errorResponse(400, "Invalid username/password");
         }
     } else {
-        exit(errorMsg(400, "User does not exsist"));
+        return errorResponse(400, "User does not exsist");
     }
 }
 
@@ -85,12 +87,12 @@ function logout($connection, $username, $session)
             updateSession($connection, $username, NULL);
 
             // RETURN SUCCESS MESSAGE
-            echo ('logged out');
+            return dataResponse(200, "Successfully logged out");
         } else {
-            exit(errorMsg(400, "Invalid user or session"));
+            return errorResponse(400, "Invalid user or session");
         }
     } else {
-        exit(errorMsg(400, "User does not exsist"));
+        return errorResponse(400, "User does not exsist");
     }
 }
 
@@ -98,7 +100,7 @@ function register($connection, $username, $email, $password, $avatar)
 {
     $user = getUserByNameOrEmail($connection, $username, $email);
     if (isset($user)) {
-        exit(errorMsg(400, "Username/email already exsists"));
+        return errorResponse(400, "Username/email already exsists");
     } else {
         $sql = isset($avatar) ?
             "INSERT INTO users (username, email, password, avatar) VALUES (?, ?, ?, ?);" :
@@ -112,7 +114,7 @@ function register($connection, $username, $email, $password, $avatar)
 
         $stmt->execute();
         // SUCCESS message
-        echo ('registered');
+        return dataResponse(200, "Successfully registered");
     }
 }
 
@@ -132,23 +134,23 @@ function forgot($connection, $email, $token, $password)
                             $stmt2->bind_param("ss", $password, $email);
                             $stmt2->execute();
                             /**** SEND CONFIRM RESPONSE ****/
-                            echo ('Update pass');
+                            return dataResponse(200, "Successfully updated password");
                         } else {
                             // token expired. reset token and token_timestamp to null
                             $sql2 = "UPDATE users SET reset_token = NULL, reset_token_timestamp = NULL WHERE email = ?;";
                             $stmt2 = $connection->prepare($sql2);
                             $stmt2->bind_param("s", $email);
                             $stmt2->execute();
-                            exit(errorMsg(400, "Password reset token expired"));
+                            return errorResponse(400, "Password reset token expired");
                         }
                     } else {
-                        exit(errorMsg(400, "Invalid reset token"));
+                        return errorResponse(400, "Invalid reset token");
                     }
                 } else {
-                    exit(errorMsg(400, "No password reset request initiated"));
+                    return errorResponse(400, "No password reset request initiated");
                 }
             } else {
-                exit(errorMsg(400, "Missing password"));
+                return errorResponse(400, "Missing password");
             }
         } else {
             $token = generateRandomString();
@@ -159,10 +161,10 @@ function forgot($connection, $email, $token, $password)
             $stmt->execute();
             // SEND RESET EMAIL SOMEHOW $token
             // SEND confirm message
-            echo ('token: ' . $token);
+            return dataResponse(200, "Token sent, check email!");
         }
     } else {
-        exit(errorMsg(400, "Email does not exsist"));
+        return errorResponse(400, "Email does not exsist");
     }
 }
 
