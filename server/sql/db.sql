00243bc88b2d4d38bd6031cdfa70a7476fd92b73
Brian Z <me@brianzhou.co>
2021-04-12 08:41:09 -0700

    Update db.sql

--- server/sql/db.sql
+++ server/sql/db.sql
@@ -5,22 +5,27 @@ CREATE TABLE `users` (
     `avatar` BLOB,
     `session` VARCHAR(255),
     `reset_token` VARCHAR(6),
-    `reset_token_timestamp` INT,
-    `enable` BOOLEAN,
+    `reset_token_timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `enable` BOOLEAN NOT NULL DEFAULT TRUE,
     PRIMARY KEY (`username`, `email`)
-) CREATE TABLE `posts` (
+);
+CREATE TABLE `posts` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(255) NOT NULL,
-    `content` VARCHAR NOT NULL,
-    `timestamp` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `views` NOT NULL DEFAULT '0',
-    PRIMARY KEY (`username`, `email`)
-) CREATE TABLE `comments` (
+    `content` TEXT NOT NULL,
+    `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `views` INT NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`username`) REFERENCES `users`(`username`)
+);
+CREATE TABLE `comments` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `post_id` INT NOT NULL,
     `username` VARCHAR(255) NOT NULL,
-    `content` VARCHAR NOT NULL,
-    `timestamp` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `views` NOT NULL DEFAULT '0',
-    PRIMARY KEY (`username`, `email`)
-)
+    `content` TEXT NOT NULL,
+    `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `views` INT NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`),
+    FOREIGN KEY (`username`) REFERENCES `users`(`username`)
+);
