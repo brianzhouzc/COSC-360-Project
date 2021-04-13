51db011745784b7f4a72ad7ace18eef461cc33f1
Brian Z <me@brianzhou.co>
2021-04-13 14:43:12 -0700

    front page auto loads via ajax

---
+++ js/frontpage.js
@@ -0,0 +1,53 @@
+var current_offset = 0;
+var posts_ids = [];
+function getPosts(order = "DESC", limit = 5, offset = 0) {
+    var data = {
+        action: "get",
+        order: order,
+        limit: limit,
+        offset: offset
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/server/posts.php",
+        data: data,
+        dataType: "json",
+        encode: true
+    }).done(function (response) {
+        if (response.hasOwnProperty('data')) {
+            //display success message
+            console.log(response.data);
+
+            response.data.posts.forEach(function (element) {
+                current_offset++;
+
+                var title = element.title;
+                var author = element.username;
+                var date = element.timestamp;
+                var content = element.content;
+                var id = element.id;
+
+                $("#post_template .post").data("id", id);
+                $("#post_template .post_title").text(title);
+                $("#post_template .post_description .post_author").text(author);
+                $("#post_template .post_description .post_date").text(date);
+                $("#post_template .post_content").text(content);
+
+                $("#post_template").children().clone().appendTo('.main');
+            });
+        } else if (response.hasOwnProperty('errors')) {
+            alert(response.errors.detail);
+        }
+    });
+}
+
+$(document).ready(function () {
+    getPosts("DESC", 5, current_offset);
+});
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        console.log("load more");
+    }
+});
