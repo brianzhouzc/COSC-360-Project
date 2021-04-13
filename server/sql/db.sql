74d81f58a0221bfe553d2be2ac462c0d4f9ee3e2
Brian Z <me@brianzhou.co>
2021-04-13 14:39:44 -0700

    fix sql

--- server/sql/db.sql
+++ server/sql/db.sql
@@ -36,14 +36,14 @@ CREATE TABLE `admins` (
 );
 
 /*We need to hardcode a few users for testing purposes--they could be admins later as well*/
-INSERT INTO users ('username','email','password','enable') VALUES ("parker616", "peter@dailybugle.com", "notspidey", 1);
-INSERT INTO users ('username','email','password','enable') VALUES ("dvader", "clanker@sithlord.com", "snips", 1);
+INSERT INTO users (username, email, password, enable) VALUES ("parker616", "peter@dailybugle.com", "notspidey", 1);
+INSERT INTO users (username, email, password, enable) VALUES ("dvader", "clanker@sithlord.com", "snips", 1);
 /*We need to hardcode posts in now that we have users to write them*/
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (0, "parker616", "This post is about my favourite place to get pizza, Joe's Pizza!", 2021-01-03, 0) WHERE 'username' = 'parker616';
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (1, "parker616", "There's nothing better than New York pizza!", 2020-02-14, 0) WHERE 'username' = 'parker616';
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (2, "parker616", "Does anyone know how to hack Stark indusrtries' firewall?", 2016-06-03, 0) WHERE 'username' = 'parker616';
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (3, "parker616", "You should rent a bike in central park!", 2019-08-14, 0) WHERE 'username' = 'parker616';
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (4, "dvader", "The First Death Star had the best pizza place in the whole galaxy. Rest in peace.", 1977-12-26, 0) WHERE 'username' = 'dvader';
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (5, "dvader", "Wow I love speederbikes.", 1983-01-07, 0) WHERE 'username' = 'dvader';
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (6, "dvader", "I'm having issues with a faulty protocol droid's memory banks. Can't read the programming language it's written in.", 1999-03-19, 0) WHERE 'username' = 'dvader';
-INSERT INTO posts ('id','username','content','timestamp','views') VALUES (7, "dvader", "I need a 'hand' coding my prothstetic arm.", 2002-03-16, 0) WHERE 'username' = 'dvader';
+INSERT INTO posts (username,title,content) VALUES ("parker616","Title 1", "This post is about my favourite place to get pizza, Joe's Pizza!");
+INSERT INTO posts (username,title,content) VALUES ("parker616", "Title 2","There's nothing better than New York pizza!");
+INSERT INTO posts (username,title,content) VALUES ("parker616", "Title 3","Does anyone know how to hack Stark indusrtries' firewall?");
+INSERT INTO posts (username,title,content) VALUES ("parker616", "Title 4","You should rent a bike in central park!");
+INSERT INTO posts (username,title,content) VALUES ("dvader", "Title 5","The First Death Star had the best pizza place in the whole galaxy. Rest in peace.");
+INSERT INTO posts (username,title,content) VALUES ("dvader", "Title 6","Wow I love speederbikes.");
+INSERT INTO posts (username,title,content) VALUES ("dvader", "Title 7","I'm having issues with a faulty protocol droid's memory banks. Can't read the programming language it's written in.");
+INSERT INTO posts (username,title,content) VALUES ("dvader", "Title 8","I need a 'hand' coding my prothstetic arm.");
