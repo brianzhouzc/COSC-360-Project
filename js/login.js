a7fe69b316948f76eb6dbdfcaaf60bc1ea7997aa
Brian Z <me@brianzhou.co>
2021-04-14 16:19:30 -0700

    Dynamically shows login/register page

--- js/login.js
+++ js/login.js
@@ -75,3 +75,7 @@ function login(username, password) {
         }
     });
 }
+
+function loadRegister(){
+    $("#mainbody").load("register.html");
+}
---
+++ js/user.js
@@ -0,0 +1,18 @@
+if ("username" in sessionStorage && "session" in sessionStorage) {
+
+} else {
+    $('#login_message_area').attr('style', '');
+    $("#main_load").load("login.html");//brings in the footer
+}
+
+function toggleLoginRegister() {
+    if ($('#login_message').text() == "Please Login") {
+        $("#main_load").load("register.html");
+        $('#login_message').text("Please Register");
+        $('#login_toggle').html('Or Login');
+    } else {
+        $("#main_load").load("login.html");
+        $('#login_message').text("Please Login");
+        $('#login_toggle').html('Or Register');
+    }
+}
