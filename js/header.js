390579bf63c429177d3dd30fc00b662496f85bdd
Brian Z <me@brianzhou.co>
2021-04-14 15:16:08 -0700

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,36 @@
+if ("username" in sessionStorage && "session" in sessionStorage) {
+    $('#avatar').attr('src', './server/avatar.php?username=' + sessionStorage.getItem('username'));
+    var link = '<a href="javascript:logout();">Logout</a>';
+    $('#info_username').text("Hello, " + sessionStorage.getItem('username'));
+    $('#info_login').html(link);
+}
+
+function logout() {
+    if ("username" in sessionStorage && "session" in sessionStorage) {
+        var data = {
+            action: "logout",
+            username: sessionStorage.getItem("username"),
+            session: sessionStorage.getItem("session")
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/server/users.php",
+            data: data,
+            dataType: "json",
+            encode: true
+        }).done(function (response) {
+            if (response.hasOwnProperty('data')) {
+                //display success message
+                sessionStorage.removeItem("username")
+                sessionStorage.removeItem("session");
+                alert(response.data.detail);
+                location.reload();
+            } else if (response.hasOwnProperty('errors')) {
+                alert(response.errors.detail);
+            }
+        });
+    } else {
+        //something's wrong
+    }
+}
