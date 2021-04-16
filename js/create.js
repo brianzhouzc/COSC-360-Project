4a01976ef91e226ce5677c178ee493464a591e80
Brian Z <me@brianzhou.co>
2021-04-15 19:55:49 -0700

    create post working

--- js/create.js
+++ js/create.js
@@ -1,4 +1,29 @@
 
-function createPost() {
+$(document).ready(function () {
+    var mainForm = document.getElementById("mainForm");
+    mainForm.onsubmit = function (e) {
+        e.preventDefault();
+        var formData = new FormData(mainForm);
+        formData.append('action', 'create');
+        formData.append('username', sessionStorage.getItem('username'));
+        formData.append('session', sessionStorage.getItem('session'));
+        $.ajax({
+            type: "POST",
+            url: "/server/posts.php",
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: "json",
+            encode: true
+        }).done(function (response) {
+            console.log(response);
+            if (response.hasOwnProperty('data')) {
+                alert(response.data.detail);
+                window.location.reload();
+            } else {
+                alert(response.errors.detail);
+            }
+        });
 
-}
+    }
+});
